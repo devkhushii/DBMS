1ca79ccd37ca47e29e33d4cd336f71373053ded3
Khushi Rani <128570886+devkhushii@users.noreply.github.com>
2026-07-01 07:37:41 +0530

    Time: 6 ms (60.58%), Space: 8.7 MB (60.9%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
-    bool isPalindrome(string s){
-        int left=0;
-        int right=s.length()-1;
-        while(left<right){
-            if(s[left]!=s[right]){
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
-    int countSubstrings(string s) {
-        int c=s.length();
+    // bool isPalindrome(string s){
+    //     int left=0;
+    //     int right=s.length()-1;
+    //     while(left<right){
+    //         if(s[left]!=s[right]){
+    //             return false;
+    //         }
+    //         left++;
+    //         right--;
+    //     }
+    //     return true;
+    // }
+    // int countSubstrings(string s) {
+    //     int c=s.length();
 
-        for(int i=0;i<s.length();i++){
-            for(int j=i+1;j<s.length();j++){
-                if(isPalindrome(s.substr(i,j-i+1))){
-                    c++;
-                }
-            }
+    //     for(int i=0;i<s.length();i++){
+    //         for(int j=i+1;j<s.length();j++){
+    //             if(isPalindrome(s.substr(i,j-i+1))){
+    //                 c++;
+    //             }
+    //         }
+    //     }
+    //     return c;
+    // }
+
+    void expand(string &s,int left,int right,int &count){
+
+        while(left>=0 && right<s.size() && s[left]==s[right]){
+            count++;
+            left--;
+            right++;
         }
-        return c;
     }
+        int countSubstrings(string s){
+            int count=0;
+
+            for(int i=0;i<s.length();i++){
+
+                expand(s,i,i,count);
+                expand(s,i,i+1,count);
+            }
+            return count;
+        }
+    
+
+
 };
